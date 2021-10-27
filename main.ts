54809ccb3a951c64d632224adec494e6b1f60340
aavelinetx <93262290+aavelinetx@users.noreply.github.com>
2021-10-27 14:09:59 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,28 @@
+function gidatu_ezkerrera () {
+    basic.showArrow(ArrowNames.North)
+    basic.showArrow(ArrowNames.NorthEast)
+    basic.showArrow(ArrowNames.East)
+    basic.showArrow(ArrowNames.SouthEast)
+    basic.showArrow(ArrowNames.South)
+    basic.showArrow(ArrowNames.SouthWest)
+    basic.showArrow(ArrowNames.West)
+    basic.showArrow(ArrowNames.NorthWest)
+}
+function gidatu_eskuinera () {
+    basic.showArrow(ArrowNames.North)
+    basic.showArrow(ArrowNames.NorthEast)
+    basic.showArrow(ArrowNames.East)
+    basic.showArrow(ArrowNames.SouthEast)
+    basic.showArrow(ArrowNames.South)
+    basic.showArrow(ArrowNames.SouthWest)
+    basic.showArrow(ArrowNames.West)
+    basic.showArrow(ArrowNames.NorthWest)
+}
+basic.forever(function () {
+    if (input.isGesture(Gesture.TiltRight)) {
+        gidatu_eskuinera()
+    }
+    if (input.isGesture(Gesture.TiltLeft)) {
+        gidatu_ezkerrera()
+    }
+})
